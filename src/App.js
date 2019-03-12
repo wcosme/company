256e781ce6e551cfb0149345d215a0a16c75e153
Wallace Gonçalves <wallacecosme@gmail.com>
2019-03-12 18:57:48 -0300

    Criação dos modulos do site

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 
 import Site from './Site'
 import Rodape from './componentes/Rodape'
-import Cabecalho from './componentes/Cabecalho';
+import Cabecalho from './componentes/Cabecalho'
+import Inicio from './componentes/Inicio'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Cabecalho />
+        <Inicio />
         <Site />
         <Rodape />
       </div>
--- src/Site.js
+++ src/Site.js
@@ -2,36 +2,7 @@ import React from 'react';
 
 const Site = props => {
   return (
-    <div>  
-
-      <div className='jumbotron text-center'>
-        <h1>Company</h1>
-        <p>We specialize in blablabla</p>
-        <form>
-          <div className='input-group'>
-            <input type='email' className='form-control' size='50' placeholder='Email Address' required />
-            <div className='input-group-btn'>
-              <button type='button' className='btn btn-danger'>Subscribe</button>
-            </div>
-          </div>
-        </form>
-      </div>
-
-      {/* Container (About Section) */}
-      <div id='about' className='container-fluid'>
-        <div className='row'>
-          <div className='col-sm-8'>
-            <h2>About Company Page</h2><br />
-            <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h4><br />
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <br /><button className='btn btn-default btn-lg'>Get in Touch</button>
-          </div>
-          <div className='col-sm-4'>
-            <span className='glyphicon glyphicon-signal logo'></span>
-          </div>
-        </div>
-      </div>
-
+    <div>
       <div className='container-fluid bg-grey'>
         <div className='row'>
           <div className='col-sm-4'>

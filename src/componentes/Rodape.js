2c4259698b0570e5571172a9a39f4e0985a5b4d1
Wallace Gonçalves <wallacecosme@gmail.com>
2019-03-12 18:47:09 -0300

    Primeiro commit da aplicação do curso

---
+++ src/componentes/Rodape.js
@@ -0,0 +1,14 @@
+import React from 'react'
+const Rodape = props => {
+    return(
+        <footer className='container-fluid text-center'>
+        <a href='#myPage' title='To Top'>
+          <span className='glyphicon glyphicon-chevron-up'></span>
+        </a>
+        <p>Site criado no curso da Udemy usando </p> 
+        <p> template Bootstrap Theme Made By <a href='https://www.w3schools.com' title='Visit w3schools'>www.w3schools.com</a></p>
+      </footer>
+    )
+}
+
+export default Rodape

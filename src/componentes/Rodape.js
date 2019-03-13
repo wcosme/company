cf1dd55434f5ff945ee1d18bcfb40a93dc76a315
Wallace Gonçalves <wallacecosme@gmail.com>
2019-03-13 16:40:13 -0300

    Alteração do componente Contato

--- src/componentes/Rodape.js
+++ src/componentes/Rodape.js
@@ -1,10 +1,7 @@
 import React from 'react'
 const Rodape = props => {
     return(
-        <footer className='container-fluid text-center'>
-        <a href='#myPage' title='To Top'>
-          <span className='glyphicon glyphicon-chevron-up'></span>
-        </a>
+        <footer className='container-fluid text-center'>        
         <p>Site criado no curso da Udemy usando </p> 
         <p> template Bootstrap Theme Made By <a href='https://www.w3schools.com' title='Visit w3schools'>www.w3schools.com</a></p>
       </footer>
